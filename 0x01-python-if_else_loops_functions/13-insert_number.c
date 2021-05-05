b4150c6ddc28a1ad3da8a78ec6d9101dd968f320
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-05-05 23:16:58 +0300

    insert num

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,36 +1,31 @@
 #include "lists.h"
-/**
- * insert_node - insert a number in a linked list
- * @head: pointer to structure
- * @number: number to be inserted
- * Return: no zero
- */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new = NULL, *ptr = NULL, *tmp = NULL;
+	listint_t *temp = NULL;
+	listint_t *new_node = NULL;
 
-	if (*head == NULL)
-		return (NULL);
-	new = (listint_t *)malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	ptr = *head;
-	if (number == 0)
+	temp = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (new_node);
+	new_node->n = number;
+	new_node->next = NULL;
+	if (*head == NULL || (*head)->n >= number)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-
-	while (ptr && ptr->n < number)
+	while (temp)
 	{
-		tmp = ptr;
-		ptr = ptr->next;
+		if (temp->next == NULL || temp->next->n >= number)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			break;
+		}
+		temp = temp->next;
 	}
-	if (ptr == NULL)
-		return (NULL);
-	new->n = number;
-	new->next = ptr;
-	tmp->next = new;
-	return (new);
+	return (new_node);
 }
