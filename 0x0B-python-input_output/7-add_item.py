918f31c68fbaf16309cee4069ff428080386489a
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-06-01 23:58:41 +0300

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""script"""
+"""module to load and save"""
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
-from sys import argv
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-try:
-    elems_list = load_from_json_file("add_item.json")
-except:
-    elems_list = []
-
-for elements in (argv[1:]):
-    elems_list.append(elements)
-    save_to_json_file(elems_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
